8aafcbb36b0e7deda5dc65df38c41f7da7de8658
oops <shreyajain15301331@gmail.com>
2026-01-29 09:15:21 +0530

    Refactor Input component and update styles

--- Input.jsx
+++ Input.jsx
@@ -1,33 +1,35 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { myAction } from './Action';
+import { myAction } from '../Component/Redus/Action';
+import './input.css';
 
 export default function Input() {
-  const [state, setState] = useState({
-    username: ""
-  });
-
+  const [state, setState] = useState({ username: "" });
   const dispatch = useDispatch();
 
   function handleText(e) {
-    setState({
-      username: e.target.value
-    });
+    setState({ username: e.target.value });
   }
 
   function handleSubmit() {
+    if (!state.username.trim()) return;
     dispatch(myAction(state));
+    setState({ username: "" });
   }
 
   return (
-    <div>
-      <input
-        type="text"
-        placeholder="Enter name"
-        onChange={handleText}
-      />
-      <button onClick={handleSubmit}>Click</button>
+    <div className="input-container">
+      <h3 className="input-title">Add New User</h3>
+
+      <div className="input-box">
+        <input
+          type="text"
+          placeholder="Enter username"
+          value={state.username}
+          onChange={handleText}
+        />
+        <button onClick={handleSubmit}>Add</button>
+      </div>
     </div>
   );
 }
-
